f2b8277a7d12ffc4ba6cb73d2580e51f27102afa
sreelakshmi <pannikuzhiyilputhenparampiloommen.1877752@studenti.uniroma1.it>
2021-11-25 21:18:18 +0100

    initial commit

---
+++ Vector3D.h
@@ -0,0 +1,73 @@
+//Vector3D.h  
+#ifndef Vector3D_h
+#define Vector3D_h
+
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+class Vector3D{
+
+ public:
+  //default constructor
+  Vector3D();
+  //Regular Constructor:
+  Vector3D(double coordinate1, double coordinate2, double coordinate3);
+  //Destructor
+  ~Vector3D() {};
+
+  //getter methods
+
+  //get the coordinates
+  double coordinates() const {return coordinate1_ ; return coordinate2_; return coordinate3_ ;}
+  //calculate the magnitude
+  double magnitude() const;
+  //Calculate theta
+  double theta() const;
+  //Calculate Phi
+  double phi() const;
+  //Scalar Product
+  double scalar_product() const;
+  //Vector Product
+  Vector3D vector_product() const;
+
+  //setter method
+  //change the coordinate values
+  void setCoordinates(double coordinate1, double coordinate2, double coordinate3){coordinate1_= coordinate1; coordinate2_ = coordinate2 ; coordinate3_ = coordinate3;} 
+
+
+  //overloading operators +,-,=
+  Vector3D operator+(const Vector3D& rhs) const;
+  Vector3D operator-(const Vector3D& rhs) const;
+  Vector3D operator=(const Vector3D& rhs) const;
+
+  //overloading  <,>
+  bool operator<(const Vector3D& rhs) const;
+  bool operator>(const Vector3D& rhs) const;
+
+  //overloading >=,<=
+  const Vector3D& operator>=(const Vector3D& rhs);
+  const Vector3D& operator<=(const Vector3D& rhs);
+  
+  //overloading * and / to multiply and divide by a float
+  Vector3D operator*(const float& rhs) const;
+  Vector3D operator/(const float& rhs) const;
+  //using friend method to achieve symmetry of the above operation
+  friend Vector3D& operator*(const float& lhs, const Vector3D& rhs);
+  friend Vector3D& operator/(const float& lhs, const Vector3D& rhs);
+
+  //overloading <<
+  friend std::ostream& operator<<(std::ostream& os, const Vector3D& rhs);
+
+  //Utility function
+  void print(const std::string& vector) const;
+
+
+ private:
+  double coordinate1_;
+  double coordinate2_;
+  double coordinate3_;
+};
+
+#endif
